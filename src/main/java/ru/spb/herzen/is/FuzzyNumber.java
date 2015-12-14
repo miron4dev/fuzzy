6eeea4cbe1a0fdbe980c7ccfc0dbf53146b4cdfe
rustock0 <rustock@lna.org.ru>
2015-12-15 00:46:53 +0300

    FuzzyNumber API has been changed. JavaDoc has been adjusted

--- src/main/java/ru/spb/herzen/is/FuzzyNumber.java
+++ src/main/java/ru/spb/herzen/is/FuzzyNumber.java
@@ -16,69 +16,79 @@ public class FuzzyNumber extends FuzzySet {
     }
 
     /**
-     * Returns a result of addition current and specified sets.
+     * Returns a result of addition current and specified numbers.
      *
-     * @param anotherNumber an another fuzzy set.
+     * @param anotherNumber an another fuzzy number.
      * @return see description.
      */
     public Map<Double, Double> add(FuzzyNumber anotherNumber) {
-        return action(anotherNumber.set, (first, second) -> first + second);
+        return action(anotherNumber, (first, second) -> first + second);
     }
 
     /**
-     * Returns a result of subtraction current and specified sets.
+     * Returns a result of subtraction current and specified numbers.
      *
-     * @param anotherNumber an another fuzzy set.
+     * @param anotherNumber an another fuzzy number.
      * @return see description.
      */
     public Map<Double, Double> subtract(FuzzyNumber anotherNumber) {
-        return action(anotherNumber.set, (first, second) -> first - second);
+        return action(anotherNumber, (first, second) -> first - second);
     }
 
     /**
-     * Returns a result of multiplication of the current and specified set.
+     * Returns a result of multiplication of the current and specified numbers.
      *
-     * @param anotherNumber an another fuzzy set.
+     * @param anotherNumber an another fuzzy number.
      * @return see description.
      */
     public Map<Double, Double> multiply(FuzzyNumber anotherNumber) {
-        return action(anotherNumber.set, (first, second) -> first * second);
+        return action(anotherNumber, (first, second) -> first * second);
     }
 
     /**
-     * Returns a result of division of the current and specified set.
+     * Returns a result of division of the current and specified numbers.
      *
-     * @param anotherNumber an another fuzzy set.
+     * @param anotherNumber an another fuzzy number.
      * @return see description.
      */
     public Map<Double, Double> divide(FuzzyNumber anotherNumber) {
-        return action(anotherNumber.set, (first, second) -> first / second);
+        return action(anotherNumber, (first, second) -> first / second);
     }
 
     /**
-     * Returns an extra maximum of the current and specified set.
+     * Returns an extra maximum of the current and specified numbers.
      *
-     * @param anotherNumber an another fuzzy set.
+     * @param anotherNumber an another fuzzy number.
      * @return see description.
      */
     public Map<Double, Double> extraMaximum(FuzzyNumber anotherNumber) {
-        return action(anotherNumber.set, Math::max);
+        return action(anotherNumber, Math::max);
     }
 
     /**
-     * Returns an extra minimum of the current and specified set.
+     * Returns an extra minimum of the current and specified numbers.
      *
-     * @param anotherNumber an another fuzzy set.
+     * @param anotherNumber an another fuzzy number.
      * @return see description.
      */
     public Map<Double, Double> extraMinimum(FuzzyNumber anotherNumber) {
-        return action(anotherNumber.set, Math::min);
+        return action(anotherNumber, Math::min);
     }
 
-    private Map<Double, Double> action(Map<Double, Double> anotherSet, BiFunction<Double, Double, Double> function) {
+    /**
+     * Returns a result of next expression:
+     * A1 action A2 = (sup {(x1, x2) | x1 function x2}; min {mu(x1), mu(x2)})
+     * where A1 is the current fuzzy number and A2 is a specified fuzzy number,
+     * and action is a specified function, e.g. A1*A2.
+     *
+     * @param anotherSet an another fuzzy number.
+     * @param function   function for calculation, e.g. A1*A2.
+     * @return see description.
+     */
+    private Map<Double, Double> action(FuzzyNumber anotherSet, BiFunction<Double, Double, Double> function) {
         Map<Double, Double> result = new TreeMap<>();
         for (Map.Entry<Double, Double> entrySet : set.entrySet()) {
-            for (Map.Entry<Double, Double> anotherEntrySet : anotherSet.entrySet()) {
+            for (Map.Entry<Double, Double> anotherEntrySet : anotherSet.set.entrySet()) {
                 double key = function.apply(entrySet.getKey(), anotherEntrySet.getKey());
                 double value = Math.min(entrySet.getValue(), anotherEntrySet.getValue());
                 if (result.containsKey(key)) {
