d441a00fdec7038b25ddb16c227928937ff1b8f8
rustock0 <rustock@lna.org.ru>
2015-12-24 11:57:19 +0300

    TriangularFuzzyNumber implementation has been adjusted for different cases

--- src/main/java/com/github/rustock0/fuzzy/TriangularFuzzyNumber.java
+++ src/main/java/com/github/rustock0/fuzzy/TriangularFuzzyNumber.java
@@ -41,15 +41,40 @@ public class TriangularFuzzyNumber {
 
     /**
      * Returns a result of multiplication current and specified numbers.
+     * 1) if (a1 > 0 and a2 > 0)
      * At(a1, alpha1, beta1) * Bt(a2, alpha2, beta2) = Ct(a1*a2, a1*alpha2 + a2*alpha1, a1*beta2 + a2*beta1)
+     * 2) if (a1 < 0 and a2 > 0)
+     * At(a1, alpha1, beta1) * Bt(a2, alpha2, beta2) = Ct(a1*a2, a2*alpha1 - a1*beta2, a2*beta1 - a1*alpha2)
+     * 3) if (a1 <0 and a2 < 0)
+     * At(a1, alpha1, beta1) * Bt(a2, alpha2, beta2) = Ct(a1*a2, -a2*beta1-a1*beta2, -a2*alpha1 - a1*alpha2)
+     * 4) else
+     * At(a1, alpha1, beta1) * Bt(a2, alpha2, beta2) = Ct(0, 0, 0)
      *
      * @param anotherNumber an another triangular fuzzy number.
      * @return see description.
      */
     public TriangularFuzzyNumber multiply(TriangularFuzzyNumber anotherNumber) {
-        double newModalValue = lowerModal * anotherNumber.lowerModal;
-        double newAlpha = lowerModal * anotherNumber.alpha + anotherNumber.lowerModal * alpha;
-        double newBeta = lowerModal * anotherNumber.beta + anotherNumber.lowerModal * beta;
+        double newModalValue;
+        double newAlpha;
+        double newBeta;
+        if (lowerModal > 0 && anotherNumber.lowerModal > 0) {
+            newModalValue = lowerModal * anotherNumber.lowerModal;
+            newAlpha = lowerModal * anotherNumber.alpha + anotherNumber.lowerModal * alpha;
+            newBeta = lowerModal * anotherNumber.beta + anotherNumber.lowerModal * beta;
+        } else if (lowerModal < 0 && anotherNumber.lowerModal > 0) {
+            newModalValue = lowerModal * anotherNumber.lowerModal;
+            newAlpha = anotherNumber.lowerModal * alpha - lowerModal * anotherNumber.beta;
+            newBeta = anotherNumber.lowerModal * beta - lowerModal * anotherNumber.alpha;
+        } else if (lowerModal < 0 && anotherNumber.lowerModal < 0) {
+            newModalValue = lowerModal * anotherNumber.lowerModal;
+            newAlpha = (-1) * anotherNumber.lowerModal * beta - lowerModal * anotherNumber.beta;
+            newBeta = (-1) * anotherNumber.lowerModal * alpha - lowerModal * anotherNumber.alpha;
+        } else {
+            //TODO?
+            newModalValue = 0;
+            newAlpha = 0;
+            newBeta = 0;
+        }
         return new TriangularFuzzyNumber(newModalValue, newAlpha, newBeta);
     }
 
@@ -62,8 +87,8 @@ public class TriangularFuzzyNumber {
      */
     public TriangularFuzzyNumber divide(TriangularFuzzyNumber anotherNumber) {
         double newModalValue = lowerModal / anotherNumber.lowerModal;
-        double newAlpha = (lowerModal * anotherNumber.beta + anotherNumber.lowerModal * alpha) / (Math.pow(anotherNumber.alpha, 2));
-        double newBeta = (lowerModal * anotherNumber.alpha + anotherNumber.lowerModal * beta) / (Math.pow(anotherNumber.alpha, 2));
+        double newAlpha = (lowerModal * anotherNumber.beta + anotherNumber.lowerModal * alpha) / (Math.pow(anotherNumber.lowerModal, 2));
+        double newBeta = (lowerModal * anotherNumber.alpha + anotherNumber.lowerModal * beta) / (Math.pow(anotherNumber.lowerModal, 2));
         return new TriangularFuzzyNumber(newModalValue, newAlpha, newBeta);
     }
 
--- src/test/java/com/github/rustock0/fuzzy/TriangularFuzzyNumberTest.java
+++ src/test/java/com/github/rustock0/fuzzy/TriangularFuzzyNumberTest.java
@@ -2,6 +2,7 @@ package com.github.rustock0.fuzzy;
 
 import org.junit.Test;
 
+import static java.lang.Double.POSITIVE_INFINITY;
 import static org.junit.Assert.*;
 
 public class TriangularFuzzyNumberTest {
@@ -25,11 +26,38 @@ public class TriangularFuzzyNumberTest {
     }
 
     @Test
-    public void testMultiply() throws Exception {
+    public void testMultiply_positive_positive() throws Exception {
         TriangularFuzzyNumber number = new TriangularFuzzyNumber(3, 1, 2);
         TriangularFuzzyNumber another = new TriangularFuzzyNumber(2, 2, 1);
 
         TriangularFuzzyNumber expected = new TriangularFuzzyNumber(6, 8, 7);
+        assertEquals(expected, number.multiply(another));
+    }
+
+    @Test
+    public void testMultiply_positive_negative() throws Exception {
+        TriangularFuzzyNumber number = new TriangularFuzzyNumber(3, 1, 2);
+        TriangularFuzzyNumber another = new TriangularFuzzyNumber(-2, 2, 1);
+
+        TriangularFuzzyNumber expected = new TriangularFuzzyNumber(6, 8, 7);
+        assertEquals(expected, number.multiply(another));
+    }
+
+    @Test
+    public void testMultiply_negative_positive() throws Exception {
+        TriangularFuzzyNumber number = new TriangularFuzzyNumber(-3, 1, 2);
+        TriangularFuzzyNumber another = new TriangularFuzzyNumber(2, 2, 1);
+
+        TriangularFuzzyNumber expected = new TriangularFuzzyNumber(-6, 5, 10);
+        assertEquals(expected, number.multiply(another));
+    }
+
+    @Test
+    public void testMultiply_negative_negative() throws Exception {
+        TriangularFuzzyNumber number = new TriangularFuzzyNumber(-3, 1, 2);
+        TriangularFuzzyNumber another = new TriangularFuzzyNumber(-2, 2, 1);
+
+        TriangularFuzzyNumber expected = new TriangularFuzzyNumber(6, 7, 8);
         assertEquals(expected, number.multiply(another));
     }
 
@@ -39,6 +67,15 @@ public class TriangularFuzzyNumberTest {
         TriangularFuzzyNumber another = new TriangularFuzzyNumber(2, 2, 1);
 
         TriangularFuzzyNumber expected = new TriangularFuzzyNumber(1.5, 1.25, 2.5);
+        assertEquals(expected, number.divide(another));
+    }
+
+    @Test
+    public void testDivide_zero() throws Exception {
+        TriangularFuzzyNumber number = new TriangularFuzzyNumber(3, 1, 2);
+        TriangularFuzzyNumber another = new TriangularFuzzyNumber(0, 2, 1);
+
+        TriangularFuzzyNumber expected = new TriangularFuzzyNumber(POSITIVE_INFINITY, POSITIVE_INFINITY, POSITIVE_INFINITY);
         assertEquals(expected, number.divide(another));
     }
 }
